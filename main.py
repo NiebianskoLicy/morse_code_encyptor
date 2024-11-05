3031c69aee6262aa0cc25de1cf8fe175f3c0cc98
NiebianskoLicy <robek0410@gmail.com>
2024-11-05 09:51:20 +0100

    adding layout of program

---
+++ main.py
@@ -0,0 +1,32 @@
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+text = input("type encrypt to encrypte, type decrypt to decrypte: ").lower()
+
+
+def encrypte(text):   # method for encrypt morse code
+    pass
+
+def decrypte(text):   # method for decrypt morse code
+    pass
+
+if text == "encrypt":
+    encrypte(text=text)
+
+elif text == "decrypt":
+    decrypte(text=text)
+else:
+    print("Try again. Use only encrypt or decrypt")
