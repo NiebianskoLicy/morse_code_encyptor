c04469e44da77438a84ed6e1a89e26c62620212f
NiebianskoLicy <robek0410@gmail.com>
2024-11-13 17:11:56 +0100

    initial commit

--- main.py
+++ main.py
@@ -53,21 +53,29 @@ def decrypt(d_text):
         if morse != " ":
             morse_code += morse
         elif morse == " " and spaces == 0:
-            spaces += 1
-            key_list = [key for key, val in MORSE_CODE_DICT.items() if val == morse_code]
-            decipher += MORSE_CODE_DICT[key_list][0]
+            if morse in MORSE_CODE_DICT["letters"]:
+                spaces += 1
+                key_list = [key for key, val in MORSE_CODE_DICT["letters"].items() if val == morse_code]
+                print(key_list)
+                decipher += MORSE_CODE_DICT["letters"][key_list][0]
+            elif morse in MORSE_CODE_DICT["else"]:
+                spaces += 1
+                key_list = [key for key, val in MORSE_CODE_DICT["else"].items() if val == morse_code]
+                print(key_list)
+                decipher += MORSE_CODE_DICT["else"][key_list][0]
         elif morse == " " and spaces == 1:
             decipher += " "
-            spaces += 1
+            spaces = 0
+    print(f"decrypted code: {decipher}")
     return decipher
 
 if method == "encrypt":
-    text_to_encrypte = input("Text to encrypte: ")
-    encrypt(e_text=text_to_encrypte)
+    text_to_encrypt = input("Text to encrypte: ")
+    encrypt(e_text=text_to_encrypt)
 
 elif method == "decrypt":
-    text_to_decrypte = input("Text to decrypte: ")
-    decrypt(d_text=text_to_decrypte)
+    text_to_decrypt = input("Text to decrypte: ")
+    decrypt(d_text=text_to_decrypt)
 
 else:
     print("Try again. Use only encrypt or decrypt")
