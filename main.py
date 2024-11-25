be0d13ad8debe63fdba5e59680eceaf3fe52694b
NiebianskoLicy <robek0410@gmail.com>
2024-11-25 17:19:03 +0100

    fixing some bugs

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 MORSE_CODE_DICT = {'letters':
                        {'A': '.-', 'B': '-...',
-                   'C': '-.-.', 'D': '-..', 'E': '.',
-                   'F': '..-.', 'G': '--.', 'H': '....',
-                   'I': '..', 'J': '.---', 'K': '-.-',
-                   'L': '.-..', 'M': '--', 'N': '-.',
-                   'O': '---', 'P': '.--.', 'Q': '--.-',
-                   'R': '.-.', 'S': '...', 'T': '-',
-                   'U': '..-', 'V': '...-', 'W': '.--',
-                   'X': '-..-', 'Y': '-.--', 'Z': '--..',}
-                   ,'else':
+                        'C': '-.-.', 'D': '-..', 'E': '.',
+                        'F': '..-.', 'G': '--.', 'H': '....',
+                        'I': '..', 'J': '.---', 'K': '-.-',
+                        'L': '.-..', 'M': '--', 'N': '-.',
+                        'O': '---', 'P': '.--.', 'Q': '--.-',
+                        'R': '.-.', 'S': '...', 'T': '-',
+                        'U': '..-', 'V': '...-', 'W': '.--',
+                        'X': '-..-', 'Y': '-.--', 'Z': '--..', }
+    , 'else':
                        {'1': '.----', '2': '..---', '3': '...--',
-                   '4': '....-', '5': '.....', '6': '-....',
-                   '7': '--...', '8': '---..', '9': '----.',
-                   '0': '-----', ',': '--..--', '.': '.-.-.-',
-                   '?': '..--..', '/': '-..-.', '-': '-....-',
-                   '(': '-.--.', ')': '-.--.-', '!': '-.-.--',
-                   '&': '.-...', ':': '---...', ';': '-.-.-.',
-                   '=': '-...-', '+': '.-.-.', '_': '..--.-',
-                   '"': '.-..-.', "$": '...-..-', '@': '.--.-.'}}
+                        '4': '....-', '5': '.....', '6': '-....',
+                        '7': '--...', '8': '---..', '9': '----.',
+                        '0': '-----', ',': '--..--', '.': '.-.-.-',
+                        '?': '..--..', '/': '-..-.', '-': '-....-',
+                        '(': '-.--.', ')': '-.--.-', '!': '-.-.--',
+                        '&': '.-...', ':': '---...', ';': '-.-.-.',
+                        '=': '-...-', '+': '.-.-.', '_': '..--.-',
+                        '"': '.-..-.', "$": '...-..-', '@': '.--.-.'}}
 
 method = input("Type encrypt or decrypt: ").lower()
 
@@ -25,7 +25,7 @@ method = input("Type encrypt or decrypt: ").lower()
 # method for encrypt morse code
 def encrypt(e_text):
     cipher = ""
-    #iterate word per word
+    # iterate word per word
     for letter in e_text:
         # adding from dictionary morse code per letter
         # one space indicate different character
@@ -35,7 +35,7 @@ def encrypt(e_text):
                 cipher += MORSE_CODE_DICT['letters'][letter.upper()] + " "
             else:
                 cipher += " "
-        else :
+        else:
             if letter != " ":
                 cipher += MORSE_CODE_DICT["else"][letter] + " "
             else:
@@ -46,6 +46,7 @@ def encrypt(e_text):
 
 # method for decrypt morse code
 def decrypt(d_text):
+    d_text += " "
     i = 0
     decipher = ""
     morse_code = ""
@@ -53,30 +54,32 @@ def decrypt(d_text):
         if morse != " ":
             i = 0
             morse_code += morse
-        else:
-            if morse_code in MORSE_CODE_DICT["letters"].values():
-                i += 1
-                if i >= 2:
-                    decipher += " "
-                else:
-                    decipher += list(MORSE_CODE_DICT["letters"].keys())[list(MORSE_CODE_DICT["letters"].values()).index(morse_code)]
+        elif morse_code in MORSE_CODE_DICT["letters"].values():
+            i += 1
+            if i >= 2:
+                decipher += " "
             else:
-                i += 1
-                if i >= 2:
-                    decipher += " "
-                else:
-                    print(decipher)
-                    decipher += list(MORSE_CODE_DICT["else"].keys())[list(MORSE_CODE_DICT['else'].values()).index(morse_code)]
-            morse_code = ""
-    print(f"decrypted code: {decipher}, {morse_code}")
+                decipher += list(MORSE_CODE_DICT["letters"].keys())[
+                    list(MORSE_CODE_DICT["letters"].values()).index(morse_code)]
+                morse_code = ""
+        else:
+            i += 1
+            if i >= 2 and morse == " ":
+                decipher += " "
+            else:
+                decipher += list(MORSE_CODE_DICT["else"].keys())[
+                    list(MORSE_CODE_DICT['else'].values()).index(morse_code)]
+                morse_code = ""
+    print(f"decrypted code: {decipher}")
     return decipher
 
+
 if method == "encrypt":
-    text_to_encrypt = input("Text to encrypte: ")
+    text_to_encrypt = input("Text to encrypt: ")
     encrypt(e_text=text_to_encrypt)
 
 elif method == "decrypt":
-    text_to_decrypt = input("Text to decrypte: ")
+    text_to_decrypt = input("Text to decrypt: ")
     decrypt(d_text=text_to_decrypt)
 
 else:
