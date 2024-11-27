139d7d1b18ada361e78a06517e9f824678634bad
NiebianskoLicy <robek0410@gmail.com>
2024-11-27 17:12:17 +0100

    initial commit

--- main.py
+++ main.py
@@ -18,20 +18,21 @@ MORSE_CODE_DICT = {'letters':
                         '(': '-.--.', ')': '-.--.-', '!': '-.-.--',
                         '&': '.-...', ':': '---...', ';': '-.-.-.',
                         '=': '-...-', '+': '.-.-.', '_': '..--.-',
-                        '"': '.-..-.', "$": '...-..-', '@': '.--.-.'}}
-# Start by selecting method
-method = input("Type encrypt or decrypt: ").lower()
+                        '"': '.-..-.', "$": '...-..-', '@': '.--.-.', ' ': ' '}}
 
 
 # method for encrypt morse code
 def encrypt(e_text):
     cipher = ""
     # iterate word per word
+    is_exit = True
     for letter in e_text:
+        if e_text == "e" or e_text == "exit":
+            exit()
         # adding from dictionary morse code per letter
         # one space indicate different character
         # two spaces indicate different words
-        if letter.upper() in MORSE_CODE_DICT["letters"]:
+        elif letter.upper() in MORSE_CODE_DICT["letters"]:
             if letter != " ":
                 cipher += MORSE_CODE_DICT['letters'][letter.upper()] + " "
             else:
@@ -47,18 +48,20 @@ def encrypt(e_text):
 
 # method for decrypt morse code
 def decrypt(d_text):
-    # adding space in the end of text helps with sliced last character
-    d_text += " "
     # counts number of spaces
     i = 0
     # deciphered text
     decipher = ""
     # putting together characters from input to prepare it for decryption
     morse_code = ""
+    is_exit = True
     for morse in d_text:
         # one space separate character, two spaces separate words
         # if its character, not a space adding it to string
-        if morse != " ":
+        if d_text.lower() == "e" or d_text == "exit":
+            print(d_text)
+            exit()
+        elif morse != " ":
             # resets spaces
             i = 0
             morse_code += morse
@@ -66,7 +69,7 @@ def decrypt(d_text):
         elif morse_code in MORSE_CODE_DICT["letters"].values():
             # counting number of spaces
             i += 1
-            if i >= 2:
+            if i == 2:
                 # adding space to separate words
                 decipher += " "
             else:
@@ -76,26 +79,44 @@ def decrypt(d_text):
                 # resets morse_code to start assemble new letter
                 morse_code = ""
         # decoding if its symbol
-        else:
+        elif morse_code in MORSE_CODE_DICT["else"].values():
             i += 1
-            if i >= 2:
+            if i == 2:
                 decipher += " "
             else:
                 decipher += list(MORSE_CODE_DICT["else"].keys())[
                     list(MORSE_CODE_DICT['else'].values()).index(morse_code)]
                 morse_code = ""
-    print(f"decrypted code: {decipher}")
-    return decipher
+        else:
+            # return to text decrypt if its not decoded text
+            print("Write only decoded text.")
+            is_exit = False
+    # false statement allow to not printing empty list
+    if is_exit:
+        print(f"decrypted code: {decipher}")
+        return decipher
 
 
-if method.lower() == "exit" or method.lower() == "e":
-    if method == "encrypt":
-        text_to_encrypt = input("Text to encrypt: ")
+# program start by selecting method
+method = input("Type encrypt or decrypt: ").lower()
+
+
+# defining new function for choosing method
+def method_choice(method_to_choose):
+    if method_to_choose == "encrypt":
+        text_to_encrypt = input("Text to encrypt (type 'e' or 'exit' to exit): ")
         encrypt(e_text=text_to_encrypt)
 
-    elif method == "decrypt":
-        text_to_decrypt = input("Text to decrypt: ")
+    elif method_to_choose == "decrypt":
+        text_to_decrypt = input("Text to decrypt(type 'e' or 'exit' to exit): ")
         decrypt(d_text=text_to_decrypt)
 
     else:
         print("Try again. Use only encrypt or decrypt")
+        return method_choice(input("Type encrypt or decrypt: ").lower())
+
+
+# creating loop allow to stay in program
+while method.lower() != "exit" or method.lower() != "e":
+    method_choice(method)
+
